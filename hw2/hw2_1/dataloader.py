bf4a2614a2fe926a398b428e0e248085a2bd713a
Derik Pack <dpack@ieee.org>
2024-10-09 23:39:11 -0400

    Uploading dataloader work

---
+++ hw2/hw2_1/dataloader.py
@@ -0,0 +1,43 @@
+import json
+from functools import cache
+from pathlib import Path
+
+import torch
+from torch.utils.data import Dataset
+from torchvision.io import read_video
+from torchvision import datasets
+import numpy as np
+
+from dictionary import sentence_to_idx
+# Maybe use vgg11 (pretrained) for the CNN?
+class MLDSVideoDataset(Dataset):
+    def __init__(self, labels_file, vid_dir, transform=None, target_transform=None):
+        # fix this
+        
+        with open(labels_file, 'r') as FILE_:
+            self.vid_labels = json.load(FILE_)
+
+        self.vid_dir = vid_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.vid_labels)
+    
+    @cache
+    def get_ids(self):
+        return [label_id for label_id in self.vid_labels]
+    
+    def __getitem__(self, idx):
+        label = self.vid_labels[idx]
+        avi_file = label['id']
+        label_caps = label['caption']
+        label_caps = [sentence_to_idx(captions) for captions in label_caps]
+        label_caps = torch.tensor(label_caps)
+        
+        #video_feature = torch.load((Path(self.vid_dir)/'feat')/f'{avi_file}.npy')
+        video_feature = np.load((Path(self.vid_dir)/'feat')/f'{avi_file}.npy')
+        video_feature = torch.from_numpy(video_feature)
+
+        return video_feature, label_caps
+    
---
+++ hw2/hw2_1/dictionary.py
@@ -0,0 +1,55 @@
+from collections import Counter
+import json
+from functools import cache
+
+@cache
+def create_dictionary(label_path):
+    dict_cnt = Counter()
+    with open(label_path,'r') as FILE_:
+        labels = json.load(FILE_)
+        for label in labels:
+            for caption in label['caption']:
+                caption = caption.replace('.','').replace('?','').replace('!','').replace(',','').replace("'",'').replace('-','').replace('"','')
+                dict_cnt.update(caption.split())
+    return dict_cnt
+
+other_tokens = {'<PAD>':0,'<BOS>':1, '<EOS>':2, '<UNK>':3 }
+
+word_dictionary = create_dictionary('training_label.json')
+# print(word_dictionary)
+word_to_idx = {key:(idx + len(other_tokens)) for idx, key in enumerate(word_dictionary) if word_dictionary[key] > 3}
+word_to_idx.update(other_tokens)
+
+
+@cache
+def get_max_length(label_path):
+    with open(label_path,'r') as FILE_:
+        labels = json.load(FILE_)
+        max_length = 0
+        for label in labels:
+            for caption in label['caption']:
+                caption = caption.replace('.','').replace('?','').replace('!','').replace(',','').replace("'",'').replace('-','').replace('"','')
+                cap_len = len(caption.split())
+                if cap_len > max_length:
+                    max_length = cap_len
+    return max_length + 2
+
+max_length=get_max_length('training_label.json')
+
+
+
+def sentence_to_idx(caption, max_length = max_length, word_to_idx=word_to_idx, other_tokens=other_tokens):
+    caption = caption.replace('.','').replace('?','').replace('!','').replace(',','').replace("'",'').replace('-','').replace('"','')
+    words = ['<BOS>'] + caption.split()
+    if len(words) < max_length:
+        words = words + ((max_length-1)-(len(words))) * ['<PAD>'] + ['<EOS>']
+    words = [word_to_idx.get(word,word_to_idx['<UNK>']) for word in words]
+    return words
+
+
+
+if __name__ == '__main__':
+    print(word_dictionary.most_common(10))
+    print(sentence_to_idx('The man ran'))
+
+    # vgg11
