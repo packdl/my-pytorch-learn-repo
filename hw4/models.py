721f936e7c8d74871cca864e5491a317097aae42
Derik <dpack@clemson.edu>
2024-11-20 13:05:33 -0500

    Minor updates to hw4. Tweaking features and other values.

--- hw4/models.py
+++ hw4/models.py
@@ -4,13 +4,16 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
-zvect = 100
-feat=32
+zvect = 100*2
+feat=80
+feat2 = 64
+# Used to be 32
 channels=3
 
 class NetG(nn.Module):
     def __init__(self):
         super(NetG,self).__init__()
+        self.embedding = nn.Embedding(10,100)
         # Adaptead from https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
         """ self.sequence = nn.Sequential(
             nn.ConvTranspose2d(zvect, feat * 4, 4, 2, 1, bias=False),
@@ -46,33 +49,46 @@ class NetG(nn.Module):
             nn.Tanh(),
         )
 
-    def forward(self, input):
-        return self.sequence(input)
+    def forward(self, inputs, label):
+        bs = inputs.size(0)
+        if len(label.size()) == 1:
+            label = label.unsqueeze(-1)
+        # print(f'{label.shape = }')
+        # embed = nn.Embedding(10, 100)
+        labels = self.embedding(label).view(bs, 100, 1,1)
+        #print(f'{inputs.shape = }')
+        #print(f'{labels.shape = }')
+        inputs = torch.concat([inputs, labels.float()], dim=1)
+        return self.sequence(inputs)
 
 class NetD(nn.Module):
     def __init__(self):
         super(NetD, self).__init__()
         self.sequence = nn.Sequential(
-            nn.Conv2d(channels, feat, 4, 2, 1, bias=False),
+            nn.Conv2d(channels, feat2, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. ``(ndf) x 32 x 32``
-            nn.Conv2d(feat, feat * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feat * 2),
+            nn.Conv2d(feat2, feat2 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feat2 * 2),
             nn.LeakyReLU(0.2, inplace=True),
+            #nn.Dropout(.4, inplace=False),
             # state size. ``(ndf*2) x 16 x 16``
-            nn.Conv2d(feat * 2, feat * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feat * 4),
+            nn.Conv2d(feat2 * 2, feat2 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feat2 * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. ``(ndf*4) x 8 x 8``
-            nn.Conv2d(feat * 4, feat * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feat * 8),
+            #nn.Dropout(.4, inplace=False),
+            nn.Conv2d(feat2 * 4, feat2 * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feat2 * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(feat * 8, 1, 2, 1, 0, bias=False),
+            #nn.Dropout(.4, inplace=False),
+            nn.Conv2d(feat2 * 8, 1, 2, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
-    def forward(self, input):
-        return self.sequence(input)
+    def forward(self, inputs):
+        return self.sequence(inputs)
+    
     
 
 if __name__ == '__main__':
@@ -84,7 +100,7 @@ if __name__ == '__main__':
     print(vals)
 
     gen = NetG()
-    out = gen(torch.rand(5,100,1,1))
+    out = gen(torch.randn(5,100,1,1), torch.randint(0,10,(5,1)))
     print(out.shape)
 
     dis = NetD()
